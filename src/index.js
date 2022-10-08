ec0ec9785ffb04ab2fabcb803aa92f74b4276077
Debanu Gupta <debanugupta@gmail.com>
2022-10-08 21:23:36 +0530

    Custom Hooks

--- src/index.js
+++ src/index.js
@@ -504,18 +504,616 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<Car />);
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./my-sass.scss";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./my-sass.scss";
 
-const Header = () => {
+// const Header = () => {
+//   return (
+//     <>
+//       <h1>Hello Style!</h1>
+//       <p>Add a little style!.</p>
+//     </>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Header />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function FavoriteColor() {
+//   const [color, setColor] = useState("red");
+
+//   return (
+//     <>
+//       <h1>My favorite color is {color}!</h1>
+//       <button type="button" onClick={() => setColor("blue")}>
+//         Blue
+//       </button>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<FavoriteColor />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Car() {
+//   const [brand, setBrand] = useState("Ford");
+//   const [model, setModel] = useState("Mustang");
+//   const [year, setYear] = useState("1964");
+//   const [color, setColor] = useState("red");
+
+//   return (
+//     <>
+//       <h1>My {brand}</h1>
+//       <p>
+//         It is a {color} {model} from {year}.
+//       </p>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Car />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Car() {
+//   const [car, setCar] = useState({
+//     brand: "Ford",
+//     model: "Mustang",
+//     year: "1964",
+//     color: "red",
+//   });
+
+//   return (
+//     <>
+//       <h1>My {car.brand}</h1>
+//       <p>
+//         It is a {car.color} {car.model} from {car.year}.
+//       </p>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Car />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Car() {
+//   const [car, setCar] = useState({
+//     brand: "Ford",
+//     model: "Mustang",
+//     year: "1964",
+//     color: "red",
+//   });
+
+//   const updateColor = () => {
+//     setCar((previousState) => {
+//       return { ...previousState, color: "blue" };
+//     });
+//   };
+
+//   return (
+//     <>
+//       <h1>My {car.brand}</h1>
+//       <p>
+//         It is a {car.color} {car.model} from {car.year}.
+//       </p>
+//       <button type="button" onClick={updateColor}>
+//         Blue
+//       </button>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Car />);
+
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Timer() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setCount((count) => count + 1);
+//     }, 1000);
+//   }, []); // <- add empty brackets here
+
+//   return <h1>I've rendered {count} times!</h1>;
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Timer />);
+
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   const [calculation, setCalculation] = useState(0);
+
+//   useEffect(() => {
+//     setCalculation(() => count * 2);
+//   }, [count]); // <- add the count variable here
+
+//   return (
+//     <>
+//       <p>Count: {count}</p>
+//       <button onClick={() => setCount((c) => c + 1)}>+</button>
+//       <p>Calculation: {calculation}</p>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Counter />);
+
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Timer() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     let timer = setTimeout(() => {
+//       setCount((count) => count + 1);
+//     }, 1000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return <h1>I've rendered {count} times!</h1>;
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Timer />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Component1() {
+//   const [user, setUser] = useState("Jesse Hall");
+
+//   return (
+//     <>
+//       <h1>{`Hello ${user}!`}</h1>
+//       <Component2 user={user} />
+//     </>
+//   );
+// }
+
+// function Component2({ user }) {
+//   return (
+//     <>
+//       <h1>Component 2</h1>
+//       <Component3 user={user} />
+//     </>
+//   );
+// }
+
+// function Component3({ user }) {
+//   return (
+//     <>
+//       <h1>Component 3</h1>
+//       <Component4 user={user} />
+//     </>
+//   );
+// }
+
+// function Component4({ user }) {
+//   return (
+//     <>
+//       <h1>Component 4</h1>
+//       <Component5 user={user} />
+//     </>
+//   );
+// }
+
+// function Component5({ user }) {
+//   return (
+//     <>
+//       <h1>Component 5</h1>
+//       <h2>{`Hello ${user} again!`}</h2>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Component1 />);
+
+// import { useState, createContext, useContext } from "react";
+// import ReactDOM from "react-dom/client";
+
+// const UserContext = createContext();
+
+// function Component1() {
+//   const [user, setUser] = useState("Jesse Hall");
+
+//   return (
+//     <UserContext.Provider value={user}>
+//       <h1>{`Hello ${user}!`}</h1>
+//       <Component2 />
+//     </UserContext.Provider>
+//   );
+// }
+
+// function Component2() {
+//   return (
+//     <>
+//       <h1>Component 2</h1>
+//       <Component3 />
+//     </>
+//   );
+// }
+
+// function Component3() {
+//   return (
+//     <>
+//       <h1>Component 3</h1>
+//       <Component4 />
+//     </>
+//   );
+// }
+
+// function Component4() {
+//   return (
+//     <>
+//       <h1>Component 4</h1>
+//       <Component5 />
+//     </>
+//   );
+// }
+
+// function Component5() {
+//   const user = useContext(UserContext);
+
+//   return (
+//     <>
+//       <h1>Component 5</h1>
+//       <h2>{`Hello ${user} again!`}</h2>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Component1 />);
+
+// import { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function App() {
+//   const [inputValue, setInputValue] = useState("");
+//   const count = useRef(0);
+
+//   useEffect(() => {
+//     count.current = count.current + 1;
+//   }, [inputValue]);
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={(e) => setInputValue(e.target.value)}
+//       />
+//       <h1>Render Count: {count.current}</h1>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useRef } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function App() {
+//   const inputElement = useRef();
+
+//   const focusInput = () => {
+//     inputElement.current.focus();
+//   };
+
+//   return (
+//     <>
+//       <input type="text" ref={inputElement} />
+//       <button onClick={focusInput}>Focus Input</button>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function App() {
+//   const [inputValue, setInputValue] = useState("");
+//   const previousInputValue = useRef("");
+
+//   useEffect(() => {
+//     previousInputValue.current = inputValue;
+//   }, [inputValue]);
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={(e) => setInputValue(e.target.value)}
+//       />
+//       <h2>Current Value: {inputValue}</h2>
+//       <h2>Previous Value: {previousInputValue.current}</h2>
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useReducer } from "react";
+// import ReactDOM from "react-dom/client";
+
+// const initialTodos = [
+//   {
+//     id: 1,
+//     title: "Todo 1",
+//     complete: false,
+//   },
+//   {
+//     id: 2,
+//     title: "Todo 2",
+//     complete: false,
+//   },
+// ];
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "COMPLETE":
+//       return state.map((todo) => {
+//         if (todo.id === action.id) {
+//           return { ...todo, complete: !todo.complete };
+//         } else {
+//           return todo;
+//         }
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+// function Todos() {
+//   const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+//   const handleComplete = (todo) => {
+//     dispatch({ type: "COMPLETE", id: todo.id });
+//   };
+
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <div key={todo.id}>
+//           <label>
+//             <input
+//               type="checkbox"
+//               checked={todo.complete}
+//               onChange={() => handleComplete(todo)}
+//             />
+//             {todo.title}
+//           </label>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Todos />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+// import Todos from "./Todos2";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [todos, setTodos] = useState([]);
+
+//   const increment = () => {
+//     setCount((c) => c + 1);
+//   };
+//   const addTodo = () => {
+//     setTodos((t) => [...t, "New Todo"]);
+//   };
+
+//   return (
+//     <>
+//       <Todos todos={todos} addTodo={addTodo} />
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useState, useCallback } from "react";
+// import ReactDOM from "react-dom/client";
+// import Todos from "./Todos3";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [todos, setTodos] = useState([]);
+
+//   const increment = () => {
+//     setCount((c) => c + 1);
+//   };
+//   const addTodo = useCallback(() => {
+//     setTodos((t) => [...t, "New Todo"]);
+//   }, [todos]);
+
+//   return (
+//     <>
+//       <Todos todos={todos} addTodo={addTodo} />
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [todos, setTodos] = useState([]);
+//   const calculation = expensiveCalculation(count);
+
+//   const increment = () => {
+//     setCount((c) => c + 1);
+//   };
+//   const addTodo = () => {
+//     setTodos((t) => [...t, "New Todo"]);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <h2>My Todos</h2>
+//         {todos.map((todo, index) => {
+//           return <p key={index}>{todo}</p>;
+//         })}
+//         <button onClick={addTodo}>Add Todo</button>
+//       </div>
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//         <h2>Expensive Calculation</h2>
+//         {calculation}
+//       </div>
+//     </div>
+//   );
+// };
+
+// const expensiveCalculation = (num) => {
+//   console.log("Calculating...");
+//   for (let i = 0; i < 1000000000; i++) {
+//     num += 1;
+//   }
+//   return num;
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useState, useMemo } from "react";
+// import ReactDOM from "react-dom/client";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [todos, setTodos] = useState([]);
+//   const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+//   const increment = () => {
+//     setCount((c) => c + 1);
+//   };
+//   const addTodo = () => {
+//     setTodos((t) => [...t, "New Todo"]);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <h2>My Todos</h2>
+//         {todos.map((todo, index) => {
+//           return <p key={index}>{todo}</p>;
+//         })}
+//         <button onClick={addTodo}>Add Todo</button>
+//       </div>
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//         <h2>Expensive Calculation</h2>
+//         {calculation}
+//       </div>
+//     </div>
+//   );
+// };
+
+// const expensiveCalculation = (num) => {
+//   console.log("Calculating...");
+//   for (let i = 0; i < 1000000000; i++) {
+//     num += 1;
+//   }
+//   return num;
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
+
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom/client";
+
+// const Home = () => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/todos")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
+
+//   return (
+//     <>
+//       {data &&
+//         data.map((item) => {
+//           return <p key={item.id}>{item.title}</p>;
+//         })}
+//     </>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Home />);
+
+import ReactDOM from "react-dom/client";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
   return (
     <>
-      <h1>Hello Style!</h1>
-      <p>Add a little style!.</p>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
     </>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<Home />);
